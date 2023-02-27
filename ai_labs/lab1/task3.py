d3115ca3241c7ac40f1efa60df694c00a1cf74f7
Aleksandr <36007931+7StaSH7@users.noreply.github.com>
2023-02-27 13:32:13 +0300

    laba1/done third task

--- ai_labs/lab1/task3.py
+++ ai_labs/lab1/task3.py
@@ -9,7 +9,7 @@ def main() -> None:
     t2 = float(input("Конец интервала: "))
     N = float(input("Кол-во точек: "))
 
-    def dpf(*, _t1: float, _t2: float, _N: float, isPlot: bool) -> float:
+    def dft(*, _t1: float, _t2: float, _N: float, isPlot: bool) -> float:
         f0 = 2000
         mvis = 4
         fdn = 2 * f0  # Частота дискретизации
@@ -68,7 +68,7 @@ def main() -> None:
 
         return exec_time
 
-    def bpf(*, _t1: float, _t2: float, _N: float, isPlot: bool) -> float:
+    def fft(*, _t1: float, _t2: float, _N: float, isPlot: bool) -> float:
         f0 = 2000
         mvis = 4
         fdn = 2 * f0  # Частота дискретизации
@@ -124,24 +124,130 @@ def main() -> None:
 
         return exec_time
 
-    def sigmoid(*, _v: NDArray[np.floating], _a: int) -> None:
-        pass
+    def threshold(*, v, a, isPlot=True):
+        x1 = list()
+        x2 = list()
+        y1 = list()
+        y2 = list()
 
-    dpf(_t1=t1, _t2=t2, _N=N, isPlot=False)
-    bpf(_t1=t1, _t2=t2, _N=N, isPlot=False)
+        for item in v:
+            if item <= a:
+                y1.append(0)
+                x1.append(item)
+            else:
+                y2.append(1)
+                x2.append(item)
 
-    dpf_time = [dpf(_t1=t1, _t2=t2, _N=2**i, isPlot=False) for i in range(15, 20)]
-    bpf_time = [bpf(_t1=t1, _t2=t2, _N=2**i, isPlot=False) for i in range(15, 20)]
-    
+        if isPlot:
+            plt.plot(x1, y1, color="r")
+            plt.plot(x2, y2, color="r")
+            plt.axhline(0, color="black", linewidth=1)
+            plt.axvline(0, color="black", linewidth=1)
+            plt.grid()
+            plt.show()
+
+        return np.concatenate([y1, y2])
+
+    def piecewise_linear(*, v, a0, a1, isPlot=True):
+        y = list()
+        x = list()
+        tan_alpha = 1 / (a1 - a0)
+
+        for item in v:
+            if item <= a0:
+                y.append(0)
+                x.append(item)
+            elif item >= a1:
+                y.append(1)
+                x.append(item)
+            else:
+                y.append(tan_alpha * (item - a1) + 1)
+                x.append(item)
+
+        if isPlot:
+            plt.axhline(0, color="black", linewidth=1)
+            plt.axvline(0, color="black", linewidth=1)
+            plt.plot(x, y, color="r")
+            plt.grid()
+            plt.show()
+
+        return y
+
+    def sigmoid(*, v, a, isPlot=True):
+        y = 1 / (1 + np.exp(-a * v))
+        if isPlot:
+            plt.axhline(0, color="black", linewidth=1)
+            plt.axvline(0, color="black", linewidth=1)
+            plt.plot(v, y, color="r")
+            plt.grid()
+            plt.show()
+
+        return y
+
+    def hyperbolic_tangent(*, v, a, isPlot=True):
+        y = np.tanh(v / a)
+        if isPlot:
+            plt.plot(v, y, color="r")
+            plt.axhline(0, color="black", linewidth=1)
+            plt.axvline(0, color="black", linewidth=1)
+            plt.grid()
+            plt.show()
+
+        return y
+
+    dft(_t1=t1, _t2=t2, _N=N, isPlot=False)
+    fft(_t1=t1, _t2=t2, _N=N, isPlot=False)
+
+    dft_time = [dft(_t1=t1, _t2=t2, _N=2**i, isPlot=False) for i in range(15, 20)]
+    fft_time = [fft(_t1=t1, _t2=t2, _N=2**i, isPlot=False) for i in range(15, 20)]
+
     plt.figure()
-    plt.plot(list(range(15, 20)), dpf_time, "-r", label="ДПФ")
-    plt.plot(list(range(15, 20)), bpf_time, "-b", label="БПФ")
+    plt.plot(list(range(15, 20)), dft_time, "-r", label="ДПФ")
+    plt.plot(list(range(15, 20)), fft_time, "-b", label="БПФ")
     plt.legend()
     plt.grid(True)
-    plt.xlabel('Data')
-    plt.ylabel('Time, s')
+    plt.xlabel("Data")
+    plt.ylabel("Time, s")
     plt.show()
 
-    v = np.arange(0, 10, 0.1)
+    v = np.arange(-8, 8, 0.05)
 
-    sigmoid(_v=v, _a=5)
+    threshold(v=v, a=3)
+    piecewise_linear(v=v, a0=3, a1=6)
+    sigmoid(v=v, a=1)
+    hyperbolic_tangent(v=v, a=1)
+
+    print("threshold: ")
+    np.column_stack((v, threshold(v=v, a=3, isPlot=False)))
+
+    print("piecewise linear: ")
+    np.column_stack((v, piecewise_linear(v=v, a0=3, a1=6, isPlot=False)))
+
+    print("sigmoid: ")
+    np.column_stack((v, sigmoid(v=v, a=1, isPlot=False)))
+
+    print("hyperbolic tangent: ")
+    np.column_stack((v, hyperbolic_tangent(v=v, a=1, isPlot=False)))
+
+    def diff(x, y):
+        d = list()
+        d.append(y[0])
+        for i in range(0, len(x) - 1):
+            d.append((y[i + 1] - y[i]) / (x[i + 1] - x[i]))
+        return d
+
+    result = diff(v, sigmoid(v=v, a=1, isPlot=False))
+
+    plt.subplot(2, 1, 1)
+    plt.plot(v, sigmoid(v=v, a=1, isPlot=False))
+    plt.grid()
+    plt.axhline(0, color="black", linewidth=1)
+    plt.axvline(0, color="black", linewidth=1)
+
+    plt.subplot(2, 1, 2)
+    plt.plot(v, result)
+    plt.grid()
+    plt.axhline(0, color="black", linewidth=1)
+    plt.axvline(0, color="black", linewidth=1)
+
+    plt.show()
