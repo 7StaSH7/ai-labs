19e289e31b1352e21a0e5fafd53c1f6f0ee89d0b
Aleksandr <sanchasyo@mail.ru>
2023-04-02 18:51:02 +0300

    lab3/some fixes

--- ai_labs/lab3/task1.py
+++ ai_labs/lab3/task1.py
@@ -8,6 +8,23 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 
+def postreg(*, title: str, T, y) -> None:
+    X_train, X_test, y_train, y_test = train_test_split(
+        y.reshape(-1, 1), T.reshape(-1, 1), test_size=1 / 3, random_state=0
+    )
+    regressor = LinearRegression()
+    regressor.fit(X_train, y_train)
+
+    # Прогнозируем результаты тестовой выборки
+    y_pred = regressor.predict(X_test)
+
+    # Визуализация результатов тестового набор данных
+    plt.figure()
+    plt.title(title)
+    plt.plot(X_test, y_test, color="blue")
+    plt.scatter(X_test, y_pred, color="red")
+
+
 def main() -> None:
     # Подготовка данных для обучения сети
     N = 1000
@@ -33,12 +50,20 @@ def main() -> None:
 
     E = model.train(input=P, target=T, epochs=1500, goal=0.01)
 
+    plt.figure()
     plt.plot(E)
     plt.grid()
-
-    y = model.sim(P)
-    print(y)
 
     p = 0.2 * np.exp(-((x - 0.8) ** 2 / 0.7))
     Y = model.sim([p])
     print(Y)
+
+    y = model.sim(P)
+
+    postreg(title="Первые выходные параметры", T=T[:, 0], y=y[:, 0])
+    postreg(title="Вторые выходные параметры", T=T[:, 1], y=y[:, 1])
+    postreg(title="Третьи выходные параметры", T=T[:, 2], y=y[:, 2])
+
+    P1 = P + np.random.uniform(0, 0.01, P.shape)
+
+    plt.show()
